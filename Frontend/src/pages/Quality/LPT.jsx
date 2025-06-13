1a769d5525eba0680304df7c865878f519080d30
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-06-13 10:29:22 +0530

    UI completed for LPT and LPT Report

--- Frontend/src/pages/Quality/LPT.jsx
+++ Frontend/src/pages/Quality/LPT.jsx
@@ -3,9 +3,41 @@ import Button from "../../components/common/Button";
 import InputField from "../../components/common/InputField";
 import Title from "../../components/common/Title";
 import Loader from "../../components/common/Loader";
+import SelectField from "../../components/common/SelectField";
 
 const LPT = () => {
+  const Temperature = [
+    { label: "Low (25°C)", value: "25" },
+    { label: "Medium (35°C)", value: "35" },
+    { label: "High (45°C)", value: "45" },
+  ];
+  const Current = [
+    { label: "Low (5 A)", value: "5" },
+    { label: "Medium (10 A)", value: "10" },
+    { label: "High (15 A)", value: "15" },
+  ];
+
   const [loading, setLoading] = useState(false);
+  const [selectedTemp, setSelectedTemp] = useState(Temperature[0]);
+  const [selectedCurr, setSelectedCurr] = useState(Current[0]);
+
+  const getCurrentShift = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+
+    // Convert current time to minutes since midnight
+    const totalMinutes = hours * 60 + minutes;
+
+    // Shift 1: 08:00 (480 minutes) to 20:00 (1200 minutes)
+    // Shift 2: 20:01 to 07:59 (the rest of the day)
+    if (totalMinutes >= 480 && totalMinutes < 1200) {
+      return { label: "Shift 1", value: "shift 1" };
+    } else {
+      return { label: "Shift 2", value: "shift 2" };
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-4 overflow-x-hidden max-w-full">
       <Title title="LPT" align="center" />
@@ -58,10 +90,59 @@ const LPT = () => {
             <InputField
               label="Current Shift"
               type="text"
+              value={getCurrentShift().label}
+              readOnly
+              className="max-w-65"
+            />
+            <SelectField
+              label="Temp"
+              options={Temperature}
+              value={selectedTemp.value || ""}
+              onChange={(e) => {
+                const selected = Temperature.find(
+                  (item) => item.value === e.target.value
+                );
+                setSelectedTemp(selected);
+              }}
+              className="w-60"
+            />
+            <SelectField
+              label="Current"
+              options={Current}
+              value={selectedCurr.value || ""}
+              onChange={(e) => {
+                const selected = Current.find(
+                  (item) => item.value === e.target.value
+                );
+                setSelectedCurr(selected);
+              }}
+              className="w-60"
+            />
+            <InputField
+              label="Set Value"
+              type="text"
               //   value={getCurrentShift().label}
               readOnly
               className="max-w-65"
             />
+            <InputField
+              label="Actual"
+              type="text"
+              //   value={getCurrentShift().label}
+              readOnly
+              className="max-w-65"
+            />
+            <Button
+              bgColor={loading ? "bg-gray-400" : "bg-blue-500"}
+              textColor={loading ? "text-white" : "text-black"}
+              className={`font-semibold h-fit mt-6 ${
+                loading ? "cursor-not-allowed" : ""
+              }`}
+              onClick={console.log("Handle Add")}
+              disabled={loading}
+            >
+              Add Defect
+            </Button>
           </div>
 
           <div className="flex flex-col md:flex-row items-start gap-1">
@@ -76,13 +157,13 @@ const LPT = () => {
                     <thead className="bg-gray-200 sticky top-0 z-10 text-center">
                       <tr>
                         <th className="px-1 py-1 border">Sr.No.</th>
-                        <th className="px-1 py-1 border">Date Time</th>
-                        <th className="px-1 py-1 border">Shift</th>
+                        <th className="px-1 py-1 border">Date</th>
                         <th className="px-1 py-1 border">Model</th>
+                        <th className="px-1 py-1 border">Shift</th>
                         <th className="px-1 py-1 border">FG Sr.No</th>
-                        <th className="px-1 py-1 border">Set Temp</th>
                         <th className="px-1 py-1 border">Category</th>
                         <th className="px-1 py-1 border">Add Defect</th>
+                        <th className="px-1 py-1 border">Set Temp</th>
                         <th className="px-1 py-1 border">Remark</th>
                       </tr>
                     </thead>
--- Frontend/src/pages/Quality/LPTReport.jsx
+++ Frontend/src/pages/Quality/LPTReport.jsx
@@ -1,7 +1,221 @@
+import { useState } from "react";
+import Button from "../../components/common/Button";
+import Loader from "../../components/common/Loader";
+import Title from "../../components/common/Title";
+import DateTimePicker from "../../components/common/DateTimePicker";
+
 const LPTReport = () => {
+  const [loading, setLoading] = useState(false);
+  const [ydayLoading, setYdayLoading] = useState(false);
+  const [todayLoading, setTodayLoading] = useState(false);
+  const [monthLoading, setMonthLoading] = useState(false);
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [reportType, setReportType] = useState("lptReport");
+  const [reportData, setReportData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [details, setDetails] = useState("");
+
   return (
-    <div>
-      <h1>LPTReport</h1>
+    <div className="min-h-screen bg-gray-100 p-4 overflow-x-hidden max-w-full">
+      <Title title="LPT Report" align="center" />
+
+      {/* Filters Section */}
+      <div className="flex gap-2">
+        <div className="bg-purple-100 border border-dashed border-purple-400 p-4 mt-4 rounded-xl grid grid-cols-1 md:grid-cols-2 gap-4 max-w-4xl items-center">
+          <DateTimePicker
+            label="Start Time"
+            name="startTime"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+          <DateTimePicker
+            label="End Time"
+            name="endTime"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+        </div>
+
+        <div className="bg-purple-100 border border-dashed border-purple-400 p-4 mt-4 rounded-xl">
+          {/* Buttons and Checkboxes */}
+          <div className="flex flex-wrap items-center gap-4">
+            <div>
+              <div className="flex flex-col gap-1">
+                <label>
+                  <input
+                    type="radio"
+                    name="reportType"
+                    value="lptReport"
+                    checked={reportType === "lptReport"}
+                    onChange={(e) => setReportType(e.target.value)}
+                  />
+                  LPT Report
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="reportType"
+                    value="dailyLptReport"
+                    checked={reportType === "dailyLptReport"}
+                    onChange={(e) => setReportType(e.target.value)}
+                  />
+                  Daily LPT Report
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="reportType"
+                    value="monthlyLptReport"
+                    checked={reportType === "monthlyLptReport"}
+                    onChange={(e) => setReportType(e.target.value)}
+                  />
+                  Monthly LPT Report
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="reportType"
+                    value="yearlyLptReport"
+                    checked={reportType === "yearlyLptReport"}
+                    onChange={(e) => setReportType(e.target.value)}
+                  />
+                  Yearly LPT Report
+                </label>
+              </div>
+            </div>
+            <div className="flex flex-col items-center gap-4">
+              <div className="flex flex-wrap gap-2">
+                <Button
+                  bgColor={loading ? "bg-gray-400" : "bg-blue-500"}
+                  textColor={loading ? "text-white" : "text-black"}
+                  className={`font-semibold ${
+                    loading ? "cursor-not-allowed" : ""
+                  }`}
+                  onClick={console.log("Handle Query")}
+                  disabled={loading}
+                >
+                  Query
+                </Button>
+                {reportData && reportData.length > 0 && (
+                  <ExportButton data={reportData} filename="LPT_Report" />
+                )}
+              </div>
+              <div className="text-left font-bold text-lg">
+                COUNT: <span className="text-blue-700">0 </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-purple-100 border border-dashed border-purple-400 p-4 mt-4 rounded-xl max-w-fit">
+          <h2 className="text-xl font-semibold text-gray-800 mb-4 text-center">
+            Quick Filters
+          </h2>
+          <div className="flex flex-wrap items-center justify-center gap-3">
+            <Button
+              bgColor={ydayLoading ? "bg-gray-400" : "bg-yellow-500"}
+              textColor={ydayLoading ? "text-white" : "text-black"}
+              className={`font-semibold ${
+                ydayLoading ? "cursor-not-allowed" : "cursor-pointer"
+              }`}
+              onClick={() => console.log("YDAY")}
+              disabled={ydayLoading}
+            >
+              YDAY
+            </Button>
+            <Button
+              bgColor={todayLoading ? "bg-gray-400" : "bg-blue-500"}
+              textColor={todayLoading ? "text-white" : "text-black"}
+              className={`font-semibold ${
+                todayLoading ? "cursor-not-allowed" : "cursor-pointer"
+              }`}
+              onClick={() => console.log("TDAY")}
+              disabled={todayLoading}
+            >
+              TDAY
+            </Button>
+            <Button
+              bgColor={monthLoading ? "bg-gray-400" : "bg-green-500"}
+              textColor={monthLoading ? "text-white" : "text-black"}
+              className={`font-semibold ${
+                monthLoading ? "cursor-not-allowed" : "cursor-pointer"
+              }`}
+              onClick={() => console.log("MTD")}
+              disabled={monthLoading}
+            >
+              MTD
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Summary Section */}
+      <div className="bg-purple-100 border border-dashed border-purple-400 p-4 mt-4 rounded-md">
+        <div className="bg-white border border-gray-300 rounded-md p-2">
+          <div className="flex flex-col md:flex-row items-start gap-1">
+            {/* Left Side */}
+            <div className="w-full md:flex-1 flex flex-col gap-2">
+              {/* Left Side Table */}
+              {loading ? (
+                <Loader />
+              ) : (
+                <div className="w-full max-h-[600px] overflow-x-auto">
+                  <table className="min-w-full border bg-white text-xs text-left rounded-lg table-auto">
+                    <thead className="bg-gray-200 sticky top-0 z-10 text-center">
+                      <tr>
+                        <th className="px-1 py-1 border">Sr.No.</th>
+                        <th className="px-1 py-1 border">Date Time</th>
+                        <th className="px-1 py-1 border">Shift</th>
+                        <th className="px-1 py-1 border">Model</th>
+                        <th className="px-1 py-1 border">Assembly No.</th>
+                        <th className="px-1 py-1 border">Set Temp</th>
+                        <th className="px-1 py-1 border">Actual Temp</th>
+                        <th className="px-1 py-1 border">Set Current</th>
+                        <th className="px-1 py-1 border">Actual Current</th>
+                        <th className="px-1 py-1 border">Set Power</th>
+                        <th className="px-1 py-1 border">Actual Power</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {/* {fpaDefect && fpaDefect.length > 0 ? (
+                        fpaDefect.map((item, index) => (
+                          <tr
+                            key={index}
+                            className="hover:bg-gray-100 text-center"
+                          >
+                            <td className="px-1 py-1 border">{item.SRNo}</td>
+                            <td className="px-1 py-1 border">
+                              {item.Date &&
+                                item.Date.replace("T", " ").replace("Z", "")}
+                            </td>
+                            <td className="px-1 py-1 border">{item.Model}</td>
+                            <td className="px-1 py-1 border">{item.Shift}</td>
+
+                            <td className="px-1 py-1 border">{item.FGSRNo}</td>
+                            <td className="px-1 py-1 border">
+                              {item.Category}
+                            </td>
+                            <td className="px-1 py-1 border">
+                              {item.AddDefect}
+                            </td>
+                            <td className="px-1 py-1 border">{item.Remark}</td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td colSpan={8} className="text-center py-4">
+                            No data found.
+                          </td>
+                        </tr>
+                      )} */}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
